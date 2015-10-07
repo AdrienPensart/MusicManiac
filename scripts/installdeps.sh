a916cc27b24740cb9e89694bb1aad62517778e47
crunch <crunchengine@gmail.com>
2015-10-06 20:46:15 -0400

    dep updated

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sudo apt-get install libfuse-dev qtbase5-dev cmake 
+sudo apt-get install clang libfuse-dev qtbase5-dev cmake uuid-dev 
+sudo update-alternatives --config c++
+
+# build taglib with clang
+
