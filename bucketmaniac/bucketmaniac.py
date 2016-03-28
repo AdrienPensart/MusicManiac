36090bab03c860b002d8323cc8f14618b27dc7f4
Adrien Pensart <adrien.pensart@corp.ovh.ca>
2016-03-28 19:40:01 -0400

    bug on artist

--- bucketmaniac/bucketmaniac.py
+++ bucketmaniac/bucketmaniac.py
@@ -26,26 +26,21 @@ def restricted_float(x,y=0.0,z=5.0):
         raise argparse.ArgumentTypeError("%r not in range [%r, %r]"%(x,y,z))
     return x
 
-def get_artist(f, default=''):
-    if 'ARTIST' not in f.tags:
+def get_first(f, tag, default=''):
+    if tag not in f.tags:
         return default
-    for item in f.tags['ARTIST']:
+    for item in f.tags[tag]:
         return str(item)
     return default
 
-def get_rating(f, default=0.0):
-    if 'FMPS_RATING' not in f.tags:
-        return default
-    for item in f.tags['FMPS_RATING']:
-        return float(item)
-    return default
+def get_artist(f, default=''):
+    return get_first(f, 'ARTIST', default)
+
+def get_rating(f, default='0.0'):
+    return get_first(f, 'FMPS_RATING', default)
 
 def get_description(f, default=''):
-    if 'DESCRIPTION' not in f.tags:
-        return default
-    for item in f.tags['DESCRIPTION']:
-        return str(item)
-    return default
+    return get_first(f, 'DESCRIPTION', default)
 
 def get_keywords(f):
     return get_description(f).split()
@@ -62,19 +57,24 @@ def duration_to_seconds(duration):
 parser = argparse.ArgumentParser(
     prog="BucketManiac",
     description="Fast playlist generator and player",
-    epilog="You'll never return iTunes")
+    epilog="You'll never return iTunes",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('folder', help='select folder to scan')
-parser.add_argument('--artist', '-a', type=str, default='', help='filter by artist')
+parser.add_argument('--artist', '-a', type=str, help='filter by artist')
 parser.add_argument('--rating', '-r', type=restricted_float, default=0.0,
     help='select minimum rating (between 0 and 5)')
 parser.add_argument('--min_duration', '--min', type=str, default=None,
     help='filter with minimum duration (xx:xx:xx)')
 parser.add_argument('--max_duration', '--max', type=str, default=None,
     help='filter with maximum duration (xx:xx:xx)')
-parser.add_argument('--plexdb', type=str, default=None, help='import playlists to Plex (need root access)')
-# sudo chmod og+w "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db"
-# sudo chmod og+rw "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db-shm"
-# sudo chmod og+rw "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db-wal"
+
+# PLEX
+default_db_path="/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db"
+parser.add_argument('--plex', action='store_true', help='import playlists to Plex (need write access to DB)')
+parser.add_argument('--plexdb', type=str, default=default_db_path, help='import playlists to Plex (need write access to DB')
+# sudo chmod og+w "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db*"
+
+parser.add_argument('--fields', type=str, help='show only these tags (artist, album, etc)')
 parser.add_argument('--noflac', action='store_true', help='disable flac loading')
 parser.add_argument('--nomp3', action='store_true', help='disable mp3 loading')
 parser.add_argument('--genm3u', action='store_true', help='autogenerate best playlists by keywords')
@@ -82,16 +82,18 @@ parser.add_argument('--withtags', type=str, default=None, help='filter by tags')
 parser.add_argument('--withouttags', type=str, default=None, help="don't select files containing tags")
 parser.add_argument('--show', action='store_true', help="show all tags")
 parser.add_argument('--play', '-p', action='store_true', help='play list of files')
-parser.add_argument('--shuffle', '--random', '-s', action='store_true', help='shuffle playlist')
+parser.add_argument('--shuffle', '--random', action='store_true', help='shuffle playlist')
 parser.add_argument('--loop', '-l', action='store_true', help='play files in loop')
 parser.add_argument('--generate', '-g', type=str, default=None, help='generate m3u playlist')
 parser.add_argument('--relative', action='store_true', help='generate playlist with relative path')
 parser.add_argument('--gui', action='store_true', help='start GUI')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+parser.add_argument('--verbose', action='store_true', help='increase verbosity level')
 args = parser.parse_args()
 
 # hide tags error in files, by default taglib print them on stderr
 #os.close(sys.stderr.fileno())
+vprint = print if args.verbose else lambda *a, **k: None
 
 formats = list()
 if not args.noflac:
@@ -103,6 +105,7 @@ musics = list()
 rating = args.rating / 5.0
 
 if args.plexdb is not None:
+    vprint("Plex DB needs relative paths")
     args.relative = True
 
 try:
@@ -120,13 +123,18 @@ if args.withouttags is not None:
     withouttags = args.withouttags.split(',')
 
 for f in files:
+    vprint('Analyzing',f.path)
     if args.min_duration is not None and f.length < minduration:
+        vprint('Bad min duration', f.length, minduration)
         continue
     if args.max_duration is not None and f.length > maxduration:
+        vprint('Bad max duration', f.length, maxduration)
         continue
-    if get_rating(f) < rating:
+    if float(get_rating(f)) < rating:
+        vprint('Bad rating', get_rating(f), rating)
         continue
-    if args.artist != '' and args.artist != f.tags['ARTIST']:
+    if args.artist is not None and args.artist != get_artist(f):
+        vprint('Bad artist', args.artist, get_artist(f))
         continue
     if args.withouttags is not None:
         found = False
@@ -135,6 +143,7 @@ for f in files:
                  found = True
                  break
         if found:
+            vprint('Bad keyword', args.withouttags, withouttags)
             continue
     if args.withtags is not None:
         notfound = False
@@ -142,10 +151,15 @@ for f in files:
             if mtag not in get_keywords(f):
                 notfound = True
         if notfound:
+            vprint('Mandatory tag not found', args.withtags, withtags)
             continue
     musics.append(f)
+    vprint('Appending',f.path)
     if args.show:
-        print(f, f.tags)
+        if args.fields is not None:
+            print([get_first(f, field.upper()) for field in args.fields.split(',')])
+        else:
+            print(f.path, f.tags, f.unsupported)
     f.close()
 
 if args.shuffle:
@@ -189,8 +203,7 @@ if args.genm3u:
             f.close()
         m3u.close()
 
-if args.plexdb is not None:
-    print('using DB',args.plexdb)
+if args.plex is not None:
     for path,name in playlists.items():
         PlexPlaylistImporter.ImportIntoPlex(path, name, args.plexdb)
 
